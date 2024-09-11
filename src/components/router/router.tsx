d3f13c800efe309465fd2fc3af6710635e06c351
jared <jaredkjar@gmail.com>
2024-09-11 15:11:11 -0600

    fix route

--- src/components/router/router.tsx
+++ src/components/router/router.tsx
@@ -5,6 +5,8 @@ import Example from "../example";
 export default function Router(props) {
   const [route, setRoute] = useState(window.location.pathname);
 
+  console.log(window.location.pathname)
+
   useEffect(() => {
     window.addEventListener("popstate", (event) => {
       setRoute(window.location.pathname);
@@ -14,10 +16,10 @@ export default function Router(props) {
 
   return (
     <>
-      {route === 'mediacloud-root-config/home' && <Example text={'Home'}/>}
-      {route === 'mediacloud-root-config/people' && <Example text={'People'}/>}
-      {route === 'mediacloud-root-config/replay' && <Example text={'Replay'}/>}
-      {route === 'mediacloud-root-config/channels' && <Example text={'Channels'}/>}
+      {route === '/mediacloud-root-config/home' && <Example text={'Home'}/>}
+      {route === '/mediacloud-root-config/people' && <Example text={'People'}/>}
+      {route === '/mediacloud-root-config/replay' && <Example text={'Replay'}/>}
+      {route === '/mediacloud-root-config/channels' && <Example text={'Channels'}/>}
     </>
   );
 }
